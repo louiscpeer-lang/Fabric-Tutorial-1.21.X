fa742d46cbf0624b2285a3cf30a6ffbb965f1a7f
louis <louiscpeer@gmail.com>
2026-02-04 21:34:20 -0500

    Added Pink Garnet and Raw Pink Garnet

--- src/main/java/net/louis/tutorialmod/TutorialMod.java
+++ src/main/java/net/louis/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.louis.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.louis.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
