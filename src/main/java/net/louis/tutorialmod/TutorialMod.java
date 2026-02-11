49df924b253f08aebf7327bc98e42a907210ebc1
louis <louiscpeer@gmail.com>
2026-02-10 20:30:37 -0500

    Added Starlight Ashes and Culiflower

--- src/main/java/net/louis/tutorialmod/TutorialMod.java
+++ src/main/java/net/louis/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.louis.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.louis.tutorialmod.block.ModBlocks;
 import net.louis.tutorialmod.item.ModItemGroups;
 import net.louis.tutorialmod.item.ModItems;
@@ -19,5 +20,7 @@ public class TutorialMod implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
 	}
 }
